f620d1c05c1ca9f5d656573d101f4d8d16ba695e
Ishagoyal <iamishagoyal@gmail.com>
2017-08-27 21:07:36 +0530

    Created simple list with name and price with a json in the js file itself

--- src/App.js
+++ src/App.js
@@ -2,8 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+// const numbers = [1,2,3,4,5];
+
+// const listItems = numbers.map((number) =>
+//    <li>{number}</li>
+//   );
+
+var data = [
+
+  {
+    "name": "Las Vegas",
+    "price" : "$5000"
+  },
+ {
+    "name": "London",
+    "price" : "$7000"
+  },
+
+  {
+    "name": "Paris",
+    "price" : "$9000"
+  }
+
+];
+
+var rows = data.map((row) =>
+  <div className="Display-items">
+   <li> {row.name} &nbsp; &nbsp;  {row.price}</li>
+   </div>
+  );
+
+
+
+class ListExample extends Component {
+
+    render() {
     return (
       <div className="App">
         <div className="App-header">
@@ -16,9 +49,14 @@ class App extends Component {
         <p>
           Initial code for airbnb clone.
         </p>
+        <div>
+        <li> Name &nbsp; &nbsp; Price </li>
+        {rows}
+        </div>
+
       </div>
     );
   }
 }
 
-export default App;
+export default ListExample;
